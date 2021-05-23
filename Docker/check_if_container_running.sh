f48d04b5117c676a251afd5ad03229f677e3b64b
AtmosOne <adil.rashitov.98@gmail.com>
2021-05-23 17:06:48 +0000

    Fix exist in case of error.

--- Docker/check_if_container_running.sh
+++ Docker/check_if_container_running.sh
@@ -1,8 +1,6 @@
-CONTAINER_NAME='bingo_main_pipelines_jupyter-notebook_1'
-
-CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+CID=$(docker ps -q -f status=running)
 if [ ! "${CID}" ]; then
   echo "Container doesn't running"
-  exit $1
+  exit 1
 fi
 unset CID
