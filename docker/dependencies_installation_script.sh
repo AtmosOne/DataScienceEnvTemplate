081a3e944f49b30899ae67afb8b864fabbc7d012
AtmosOne <adil.rashitov.98@gmail.com>
2021-06-26 18:29:48 +0000

    UPDATE: remove dependency from private repository.

--- docker/dependencies_installation_script.sh
+++ docker/dependencies_installation_script.sh
@@ -1,5 +1,4 @@
 pip3 install --upgrade pip \
     && pip3 install wheel \
     && pip3 install -r /requirements.txt \
-    && pip3 install git+https://github.com/AtmosOne/S3SDK.git \
-    && pip3 install git+https://${GITHUB_LOGIN}:${GITHUB_PASSWORD}@github.com/WasteLabs/GPSOdyssey.git
+    && pip3 install git+https://github.com/AtmosOne/S3SDK.git
